cf445bf4d15336f6cab332e7e8a33599b2638bcd
Emily <emily.a.harber@gmail.com>
2020-02-04 22:02:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+function mapToNegativize(array){
+  let negs = []
+  array.forEach(elem => {
+   negs.push( elem - (elem * 2) )
+  });
+  return negs
+}
+
+function mapToNoChange(array){
+  return array
+}
+
+function mapToDouble(array){
+  let doubled = []
+  array.forEach( elem => {
+    doubled.push( elem * 2 )
+  })
+  return doubled
+}
+
+function mapToSquare(array){
+  let square = []
+  array.forEach( elem => {
+    square.push( elem ** 2 )
+  })
+  return square
+}
+
+function reduceToTotal(array, start = 0){
+  let total = start 
+  array.forEach( elem => {
+    total += elem
+  })
+  return total 
+}
+
+function reduceToAllTrue(array){
+  let result = true 
+  array.forEach( elem => {
+    if ( elem == false ) { return result = false }
+  }) 
+  return result 
+}
+
+function reduceToAnyTrue(array){
+  let result = false 
+  array.forEach( elem => {
+    if( elem == true ) { return result = true }
+  })
+  return result 
+}
